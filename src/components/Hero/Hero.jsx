1429f79ee8f0b6f5f188778b656adf5311f7b25c
OLALEKAN OGUNDELE <Olalekan Ogundele@oakbank.com>
2023-07-31 17:27:29 +0100

    Hero done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Hero.css'
+import CountUp from "react-countup";
 const Hero = () =>{
     return(  
         <section className="hero-wrapper">
@@ -7,6 +8,7 @@ const Hero = () =>{
 
                 <div className="flexColStart hero-left">
                   <div className="hero-title">
+                    <div className="orange-circle"/>
                     <h1>
                         Discover <br/> 
                         More <br/>
@@ -19,6 +21,54 @@ const Hero = () =>{
                     which makes us STAY with you over a life time.
                     </span>
                   </div>
+                  <div className="innerWidth hero-button">
+                    <div>
+                  <button className="button">
+                    <a href="">View Properties</a>
+                    </button>
+                    </div>
+                    <div>
+                    <button className="button">
+                    <a href="">Get Started</a>
+                    </button>
+                    </div>
+                  </div>
+
+                  
+                  <div className="flexCenter stats">
+                  <div className="flexColStart hero-stats">
+                    <span>
+                    <CountUp start={30} end={50} duration={4}/> <span>+</span>
+                    </span>
+                   
+                    <span>
+                    For Sale
+                    </span>
+                      
+                  </div>
+                  <div className="flexColStart hero-stats">
+                    <span>
+                    <CountUp start={15} end={25} duration={4}/> <span>+</span>
+                    </span>
+                   
+                    <span>
+                    UpComing Projects
+                    </span>
+                      
+                  </div>
+                  <div className="flexColStart hero-stats">
+                    <span>
+                    <CountUp start={15} end={25} duration={4}/> <span>+</span>
+                    </span>
+                   
+                    <span>
+                    Sold out
+                    </span>
+                      
+                  </div>
+                  </div>
+
+   
                 </div>
                 <div className="flexCenter hero-right">
                     <div className="image-container">

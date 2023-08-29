7342e6760b4cda8dae7d3c6f56985b6a6648454b
OLALEKAN OGUNDELE <Olalekan Ogundele@oakbank.com>
2023-08-29 16:46:55 +0100

    Property page hero done

--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -4,13 +4,16 @@ import "./Properties.css"
 // import useProperties from '../../hooks/useProperties'
 import PropertyCard from '../../components/PropertyCard/PropertyCard'
 import data from "../../utils/slider.json"
+import Heroprojects from '../../components/Heroprojects/Heroprojects'
 
 const Properties = () => {
     // const {data, isError, isLoading} = useProperties()
   return (
 
     <div className='wrapper'>
+       <Heroprojects/>
         <div className="flexColCenter paddings innerWidth properties-container">
+       
         <SearchBar />
         <div className="paddings flexCenter properties">
           {
